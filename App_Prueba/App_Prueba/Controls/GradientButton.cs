d4f28dcf0cf56f66400d53a7ae335609901dbf42
Wilmilcard <leonjuandavid@hotmail.com>
2021-10-16 18:24:46 -0500

    Instalacion de SkiaSharp

---
+++ App_Prueba/App_Prueba/Controls/GradientButton.cs
@@ -0,0 +1,11 @@
+﻿using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Prueba.Controls
+{
+    public class GradientButton : SKCanvasView
+    {
+    }
+}
