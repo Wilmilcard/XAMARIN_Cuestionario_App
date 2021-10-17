9cdf6390deaaef82115ac3146590ea76b26e282e
Wilmilcard <leonjuandavid@hotmail.com>
2021-10-17 10:13:34 -0500

    FontAwesome

--- App_Prueba/App_Prueba/Views/PrincipalView.xaml.cs
+++ App_Prueba/App_Prueba/Views/PrincipalView.xaml.cs
@@ -19,9 +19,14 @@ namespace App_Prueba.Views
             InitializeComponent();
         }
 
-        async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new GameView()));
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new CreditsView()));
         }
     }
 }
