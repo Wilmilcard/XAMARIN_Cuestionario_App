2ca7df3dac17f042ce2f6ff4e322612a59d60a98
Wilmilcard <leonjuandavid@hotmail.com>
2021-10-18 23:19:58 -0500

    BoolToConverters

---
+++ App_Prueba/App_Prueba/Clases/BoolToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App_Prueba.Clases
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool ParamRandom;
+
+            bool.TryParse(value.ToString(), out ParamRandom);
+
+            if (ParamRandom)
+                return Color.FromHex("#28B463");
+            return Color.FromHex("#FF5733");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new object();
+        }
+    }
+}
---
+++ App_Prueba/App_Prueba/Clases/BoolToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App_Prueba.Clases
+{
+    public class BoolToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool ParamRandom;
+
+            bool.TryParse(value.ToString(), out ParamRandom);
+
+            if (ParamRandom)
+                return "\uf00c";
+            return "\uf00d";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new object();
+        }
+    }
+}
