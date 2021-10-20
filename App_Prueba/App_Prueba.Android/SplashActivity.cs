155a7ee144fd3bf5f5382329056f78709986ec10
Wilmilcard <leonjuandavid@hotmail.com>
2021-10-19 19:10:39 -0500

    SplashScreen

---
+++ App_Prueba/App_Prueba.Android/SplashActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App_Prueba.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
